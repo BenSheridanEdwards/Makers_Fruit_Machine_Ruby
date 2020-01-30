e369e160a4e4e5489781708c4e83f960f662a931
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-30 18:08:44 +0000

    Removing print out of method in fruit machine test

--- spec/fruit_machine_spec.rb
+++ spec/fruit_machine_spec.rb
@@ -43,7 +43,6 @@ describe FruitMachine do
       machine.insert(1)
       expect(Slots).to receive(:random).and_return(["Black", "White", "White", "Yellow"])
       machine.pull_lever
-      p machine.game_adjacent_colour?
       expect(machine.player_wins?).to be true
     end
   end
